d148f7bff4ec84add4fefa9e43a71f34436f7e74
SEBPort0 <sebassi@live.com>
2019-04-05 17:37:15 -0300

    js-calculator: css styling(wip), logic needs more work.

--- js-calculator/src/index.js
+++ js-calculator/src/index.js
@@ -7,8 +7,10 @@ function Key(props) {
 		<button 
 			type="button"
 			id={props.id}
-		>
-			{props.id}
+			value={props.value}
+			onClick={props.onClick}
+		>	
+			{props.value}
 		</button>
 	);
 }
@@ -20,23 +22,31 @@ class Keyboard extends React.Component {
 							0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
 					 		10, 11, 12, 13, 14, 15, 16
 		];
-		this.symbols = [
-							"0", "1", "2", "3", "4", "5",
+		this.symbols = [	"0", "1", "2", "3", "4", "5",
 							"6", "7", "8", "9", "=",
-							"+", "-", "x", "/", 
+							"+", "-", "*", "/", 
 							".", "AC"
+		];
+		this.ids = [	"zero", "one", "two", "three", "four", "five",
+						"six", "seven", "eight", "nine", "equals",
+						"add", "subtract", "multiply", "divide",
+						"decimal", "clear"
 		];
 		this.keys = this.numbers.map(num => 
 			<Key 
 				key={num}
-				id={this.symbols[num]}
+				value={this.symbols[num]}
+				id={this.ids[num]}
+				onClick={this.props.handleClick}
 			/>);
 	}
-	
+
 	render() {
 		return(
-			<div className="keyboard">
-				{this.keys}
+			<div className="keyboard-container">
+				<div className="keyboard-keys">
+					{this.keys}
+				</div>
 			</div>
 		);
 	}	
@@ -48,46 +58,66 @@ function Display(props) {
 			id="display"
 			className="display"
 		>
-			<div className="display-output">
-				{props.output}
-			</div>
-			<div className="display-input">
-				{props.input}
-			</div>
+			{props.formula}
+			{props.ans}
 		</div>
 	);
-}
-
-function Processor(props) {
-	return(null);
 }
 
 class Calculator extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-						input: "",
-					  	output: 0,
-					  	equation: ""
+		this.handleClick = this.handleClick.bind(this);
+		this.handleClear = this.handleClear.bind(this);
+		this.handleDecimal = this.handleDecimal.bind(this);
+		this.state = {	current: "0",
+					  	formula: "",
+						ans: 0,
+						lastClicked: ""
 		};
 	}
 
-	reset() {
-		console.log("reset!")
-		this.setState({
-						input: "", 
-						ouput: 0
+	handleClick(event) {
+		const numbers = [	"one", "two", "three", "four", "five",
+							"six", "seven", "eight", "nine", "zero"
+		];
+		const operators = [	"add", "subtract", "multiply", "divide"	];
+		const {id, value} = event.target;
+
+		if(id === "clear") {
+			this.handleClear();
+		} else if(id === "equals") {
+			this.setState({ ans: eval(this.state.formula) });
+			this.setState({ formula: "" });
+		} else if(id === "decimal") {
+			this.handleDecimal();
+		} else {
+			this.setState(prevState => ({ formula: 
+				prevState.formula + value }));
+		}
+		this.setState({ lastClicked: value })
+	}
+
+	handleClear() {
+		this.setState({ current: "0",
+					  	formula: "",
+						ans: 0,
+						lastClicked: "", 
 		});
-	}	
+	}
+
+	handleDecimal() {
+		return undefined;
+	}
 
 	render() {
 		return(
-			<div>
-				<Keyboard />
+			<div className="calculator">
 				<Display 
-					input={this.state.input} 
-					output={this.state.output}
+					ans={this.state.ans}
+					formula={this.state.formula}
 				/>
+				<Keyboard handleClick={this.handleClick}/>
 			</div>
 		);
 	}
