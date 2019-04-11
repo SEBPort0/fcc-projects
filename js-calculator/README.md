0163f409ebe9b97b9c6c0ca388e013832e4fc87d
SEBPort0 <sebassi@live.com>
2019-04-11 17:50:01 -0300

    js-calculator: debugging needed.

--- js-calculator/README.md
+++ js-calculator/README.md
@@ -1,16 +1,26 @@
+# JS Calculator
+
+[Curriculum](https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-javascript-calculator)
+
+## Structure
+
+```
+  Calculator
+      |
+      |__ Keyboard
+      |		|
+      |		|__ Keys
+      |		
+      |__ Display
+  
+``` 
+
+## 	Interesting aspects of the project
+
+- Another step towards a "complete app".
 
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
 
--- js-calculator/src/index.js
+++ js-calculator/src/index.js
@@ -2,67 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Key(props) {
-	return(
-		<button 
-			type="button"
-			id={props.id}
-			value={props.value}
-			onClick={props.onClick}
-		>	
-			{props.value}
-		</button>
-	);
-}
-
-class Keyboard extends React.Component {
-	constructor(props) {
-		super(props);
-		this.numbers = [
-							0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-					 		10, 11, 12, 13, 14, 15, 16
-		];
-		this.symbols = [	"+", "-", "*", "/",
-							"0", "1", "2", "3", "4", "5",
-							"6", "7", "8", "9", "=", 
-							".", "AC"
-		];
-		this.ids = [	"add", "subtract", "multiply", "divide",
-						"zero", "one", "two", "three", "four", "five",
-						"six", "seven", "eight", "nine", "equals",
-						"decimal", "clear"
-		];
-		this.keys = this.numbers.map(num => 
-			<Key 
-				key={num}
-				value={this.symbols[num]}
-				id={this.ids[num]}
-				onClick={this.props.handleClick}
-			/>);
-	}
-
-	render() {
-		return(
-			<div className="keyboard-container">
-				<div className="keyboard-keys">
-					{this.keys}
-				</div>
-			</div>
-		);
-	}	
-}
-
-function Display(props) {
-	return(
-		<div 
-			id="display"
-			className="display"
-		>
-		  {props.current}
-		</div>
-	);
-}
-
 class Calculator extends React.Component {
 	constructor(props) {
 		super(props);
@@ -77,6 +16,19 @@ class Calculator extends React.Component {
 						lastClicked: "",
 						decimal: false
 		};
+	}
+ 
+	render() {
+		return(
+      		<div>
+				<div className="calculator">
+					<Display current={this.state.current} />
+					<Keyboard handleClick={this.handleClick}/>
+        		</div>
+      
+        		<Signature />
+      		</div>
+		);
 	}
 
 	handleClick(event) {
@@ -155,17 +107,84 @@ class Calculator extends React.Component {
 	handleDecimal() {
 		return undefined;
 	}
+}
+
+function Key(props) {
+	return(
+		<button 
+			type="button"
+			id={props.id}
+			value={props.value}
+			onClick={props.onClick}
+		>	
+			{props.value}
+		</button>
+	);
+}
+
+class Keyboard extends React.Component {
+	constructor(props) {
+		super(props);
+		this.numbers = [
+							0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					 		10, 11, 12, 13, 14, 15, 16
+		];
+		this.symbols = [	"+", "-", "*", "/",
+							"0", "1", "2", "3", "4", "5",
+							"6", "7", "8", "9", "=", 
+							".", "AC"
+		];
+		this.ids = [	"add", "subtract", "multiply", "divide",
+						"zero", "one", "two", "three", "four", "five",
+						"six", "seven", "eight", "nine", "equals",
+						"decimal", "clear"
+		];
+		this.keys = this.numbers.map(num => 
+			<Key 
+				key={num}
+				value={this.symbols[num]}
+				id={this.ids[num]}
+				onClick={this.props.handleClick}
+			/>);
+	}
 
 	render() {
 		return(
-			<div className="calculator">
-				<Display current={this.state.current} />
-				<Keyboard handleClick={this.handleClick}/>
+			<div className="keyboard-container">
+				<div className="keyboard-keys">
+					{this.keys}
+				</div>
 			</div>
 		);
-	}
+	}	
 }
 
-//-----------------------------------------------------------------------
+function Display(props) {
+	return(
+		<div 
+			id="display"
+			className="display"
+		>
+		  {props.current}
+		</div>
+	);
+}
+
+function Signature(props) {
+	return(
+	  <footer className="sebport0-signature">
+        By <i className="fab fa-github"></i>
+          <a 
+            href="https://github.com/sebport0"
+            target="_blank"
+          >
+            sebport0
+          </a>
+       </footer>
+	);
+}
+
+
+//---------------------------------------------------------------------
 
 ReactDOM.render(<Calculator />, document.getElementById("root"));
